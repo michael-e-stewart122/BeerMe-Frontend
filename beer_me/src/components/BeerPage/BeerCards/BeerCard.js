7af4d00e1822730c4344ef34fd5119be4d1a3503
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-05-09 23:17:51 -0700

    added redux and beer page'

---
+++ beer_me/src/components/BeerPage/BeerCards/BeerCard.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import {
+  Row,
+  Col,
+  Card,
+  CardTitle,
+  CardImg,
+  CardBody,
+  CardText,
+  CardSubtitle,
+  Button
+} from 'reactstrap';
+
+const BeerCard = props => {
+  let { id, beer_name, style, abv, ibu } = props.beer;
+  console.log(props.beer);
+  return (
+    <Col sm="4">
+      <Card>
+        <CardImg
+          top
+          src="http://microbrewr.com/wp-content/uploads/2014/11/21st-amendment-logo.jpg"
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardTitle>{beer_name}</CardTitle>
+          <CardSubtitle>{style}</CardSubtitle>
+          {/* <CardText>located at blah blah blah</CardText> */}
+          <Button>Discover</Button>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+};
+
+const mapStateToProps = (state, props) => ({
+  beers: state.beers
+});
+
+export default connect(mapStateToProps)(BeerCard);
---
+++ beer_me/src/redux/actions/beers.js
@@ -0,0 +1,105 @@
+export const FETCH_BEERS_SUCCESS = 'FETCH_BEERS_SUCCESS';
+export const FETCH_BEERS_FAILED = 'FETCH_BEERS_FAILED';
+
+// export const CREATE_BEER_SUCCESS = 'CREATE_BEER_SUCCESS';
+// export const CREATE_BEER_FAILED = 'CREATE_BEER_FAILED';
+//
+// export const INCREASE_VOTES_SUCCESS = 'INCREASE_VOTES_SUCCESS';
+// export const INCREASE_VOTES_FAILED = 'INCREASE_VOTES_FAILED';
+//
+// export const DECREASE_VOTES_SUCCESS = 'DECREASE_VOTES_SUCCESS';
+// export const DECREASE_VOTES_FAILED = 'DECREASE_VOTES_FAILED';
+//
+// export const FILTER_BEER_TITLE = 'FILTER_BEER_TITLE';
+
+const BASE_URL = 'http://localhost:8000';
+export const fetchBeers = () => {
+  return async dispatch => {
+    try {
+      let response = await fetch(`${BASE_URL}/beers`);
+      let beers = await response.json();
+      dispatch({
+        type: FETCH_BEERS_SUCCESS,
+        payload: beers
+      });
+    } catch (err) {
+      dispatch({
+        type: FETCH_BEERS_FAILED,
+        payload: err
+      });
+    }
+  };
+};
+//
+// export const createPost = newPost => {
+//   return async dispatch => {
+//     try {
+//       let response = await fetch(`${BASE_URL}/beers`, {
+//         method: 'BEER',
+//         body: JSON.stringify(newPost),
+//         headers: { 'Content-Type': 'application/json' }
+//       })
+//       let post = await response.json()
+//       dispatch({
+//         type: CREATE_BEER_SUCCESS,
+//         payload: post
+//       })
+//       console.log(post)
+//     } catch (err) {
+//       dispatch({
+//         type: CREATE_BEER_FAILED,
+//         payload: err
+//       })
+//       console.log(err)
+//     }
+//   }
+// }
+//
+// export const increaseVotes = id => {
+//   return async dispatch => {
+//     try {
+//       let response = await fetch(`${BASE_URL}/posts/votes/increase/${id}`)
+//       let post = await response.json()
+//       console.log(post)
+//       dispatch({
+//         type: INCREASE_VOTES_SUCCESS,
+//         payload: post
+//       })
+//     } catch (err) {
+//       console.log(err)
+//       dispatch({
+//         type: INCREASE_VOTES_FAILED,
+//         payload: err
+//       })
+//     }
+//   }
+// }
+//
+// export const decreaseVotes = id => {
+//   return async dispatch => {
+//     try {
+//       let response = await fetch(`${BASE_URL}/posts/votes/decrease/${id}`)
+//       let post = await response.json()
+//       console.log(post)
+//       dispatch({
+//         type: DECREASE_VOTES_SUCCESS,
+//         payload: post
+//       })
+//     } catch (err) {
+//       console.log(err)
+//       dispatch({
+//         type: DECREASE_VOTES_FAILED,
+//         payload: err
+//       })
+//     }
+//   }
+// }
+//
+// export const filterPosts = str => {
+//   return dispatch => {
+//     dispatch({
+//       type: FILTER_BEER_TITLE,
+//       payload: str
+//     });
+//   };
+// };
