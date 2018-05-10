3406364cc83f0ec49224db37ff21867e3a5c185a
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-05-09 18:19:22 -0700

    hey

--- beer_me/src/components/Main/Main.js
+++ beer_me/src/components/Main/Main.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Cards from '../Cards/Cards';
+
 import { Jumbotron, Container, Row, Col, Button } from 'reactstrap';
 
 class Main extends Component {
   render() {
     return (
       <Container>
-        <Row className="pr-0" sm="9">
+        {/* <Row> */}
+        {/* <Carousel /> */}
+        {/* </Row> */}
+        <Row>
           <Cards />
         </Row>
       </Container>
