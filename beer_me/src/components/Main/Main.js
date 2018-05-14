60bb0e98a30e279e8842bbf38720a3efa41d615d
Jonathan Riemer <jriemer@Jonathans-MacBook-Pro-2.local>
2018-05-13 18:19:13 -0700

    carousel

--- beer_me/src/components/Main/Main.js
+++ beer_me/src/components/Main/Main.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Cards from '../Cards/Cards';
-import Carousel from '../Carousel/Carousel';
+import SlideShow from '../Carousel/SlideShow';
+import '../Carousel/Carousel.css';
+
 import { Jumbotron, Container, Row, Col, Button } from 'reactstrap';
 
 class Main extends Component {
   render() {
     return (
       <Container>
-        <Row>
-          <Carousel />
+        <Row lg="12">
+          <Col minHeight="100%" lg="12">
+            <SlideShow />
+          </Col>
         </Row>
         <Row>
           <Cards />
