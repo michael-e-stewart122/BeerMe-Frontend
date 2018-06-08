85a9d16a0fc6f3392c391d7e9d28702fc088fe5e
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-06-08 11:36:12 -0700

    heroku bug

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
-if ((process.env.NODE_ENV = 'production')) {
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-} else {
-  const API_BASE_URL = 'http://localhost:8000';
-}
+// if ((process.env.NODE_ENV = 'production')) {
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// } else {
+// const API_BASE_URL = 'http://localhost:8000';
+// }
 exports.default = API_BASE_URL;
