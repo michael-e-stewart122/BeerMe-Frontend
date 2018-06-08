5d10523a8c17a792a83fc18ae2a53a80932acf7d
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-06-08 13:42:45 -0700

    user beers

--- src/redux/reducers/auth.reducers.js
+++ src/redux/reducers/auth.reducers.js
@@ -28,7 +28,7 @@ export default (state = initialState, action) => {
         ...state,
         user: action.payload.user,
         token: action.payload.token,
-        userBeers: action.payload.user.favorite_beers,
+        userBeers: action.payload.userBeers,
         isLoggedIn: true,
         isLoading: false
       };
