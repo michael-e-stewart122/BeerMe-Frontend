8059c2442d6a93405c83b226b0e0820e28576331
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-06-13 14:03:55 -0700

    removed login and signup folders

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import Main from './components/Main/Main';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import Main from './components/Main/Main'
 // signup/login
-import auth_actions from './redux/actions/auth_actions';
+import auth_actions from './redux/actions/auth_actions'
 // api calls to beers and breweries
-import { getAuth } from './redux/actions/auth_actions';
-import { fetchBeers } from './redux/actions/beers';
-import { fetchBreweries } from './redux/actions/breweries';
+import { getAuth } from './redux/actions/auth_actions'
+import { fetchBeers } from './redux/actions/beers'
+import { fetchBreweries } from './redux/actions/breweries'
 // beer and brewery components
-import LoginPageContainer from './redux/containers/LoginPageContainer';
-import Signup from './components/Signup/Signup';
-import BeerPage from './components/BeerPage/BeerPage';
-import FetchBeerPage from './components/FetchBeerPage/FetchBeerPage';
-import BreweryPage from './components/BreweryPage/BreweryPage';
-import FetchBreweryPage from './components/FetchBreweryPage/FetchBreweryPage';
-import ProfilePage from './components/ProfilePage/ProfilePage';
+
+import BeerPage from './components/BeerPage/BeerPage'
+import FetchBeerPage from './components/FetchBeerPage/FetchBeerPage'
+import BreweryPage from './components/BreweryPage/BreweryPage'
+import FetchBreweryPage from './components/FetchBreweryPage/FetchBreweryPage'
+import ProfilePage from './components/ProfilePage/ProfilePage'
 // redux store and routers
 
-import setupStore from './redux/store';
+import setupStore from './redux/store'
 
-const store = setupStore();
+const store = setupStore()
 
-store.dispatch(fetchBeers());
-store.dispatch(getAuth());
-store.dispatch(fetchBreweries());
+store.dispatch(fetchBeers())
+store.dispatch(getAuth())
+store.dispatch(fetchBreweries())
 
 export default class App extends Component {
   render() {
@@ -44,10 +43,8 @@ export default class App extends Component {
                 path="/"
                 component={() => <Redirect to="/cheers" />}
               />
-              <Route exact path="/login" component={LoginPageContainer} />
               <Route path="/cheers" component={Main} />
               <Route exact path="/beers" component={BeerPage} />
-              <Route path="/signup" component={Signup} />
               <Route path="/profile" component={ProfilePage} />
               <Route exact path="/breweries" component={BreweryPage} />
               <Route exact path="/beers/:id" component={FetchBeerPage} />
@@ -57,12 +54,12 @@ export default class App extends Component {
           </Router>
         </Provider>
       </div>
-    );
+    )
   }
 }
 
 const mapDispatchToProps = () => {
-  return { auth: auth_actions.getAuth() };
-};
+  return { auth: auth_actions.getAuth() }
+}
 
-connect(mapDispatchToProps)(App);
+connect(mapDispatchToProps)(App)
